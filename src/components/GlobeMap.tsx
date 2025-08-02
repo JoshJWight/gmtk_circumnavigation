092d68ef1bbb27d1933ba5abc42278428c220b2c
Josh Wight <joshjwight@gmail.com>
2025-08-02 10:32:42 -0400

    react-globe.gl imported

--- src/components/GlobeMap.tsx
+++ src/components/GlobeMap.tsx
@@ -1,8 +1,16 @@
 import React, {createContext, useContext, useEffect, useState} from 'react';
+import Globe from 'react-globe.gl';
 import './GlobeMap.css';
 import type {GameState, Flight} from '../gameTick';
 import {durationDisplayString, clockDisplayString} from '../gameTick';
 import { useGameState, GameContext } from '../gameContext';
+
+type MarkerData = {
+    lat: number;
+    lng: number;
+    size: number;
+    color: string;
+};
 
 export const GlobeMap: React.FC<{ gameState: GameState; updateGameState: (newState: Partial<GameState>) => void }> = ({ gameState, updateGameState}) => {
 
@@ -56,7 +64,21 @@ export const GlobeMap: React.FC<{ gameState: GameState; updateGameState: (newSta
     const setSimSpeed = (speed: number) => {
         updateGameState({ simSpeed: speed });
     }
-    
+
+    const markerSvg = `<svg viewBox="-4 0 36 36">
+        <path fill="currentColor" d="M14,0 C21.732,0 28,5.641 28,12.6 C28,23.963 14,36 14,36 C14,36 0,24.064 0,12.6 C0,5.641 6.268,0 14,0 Z"></path>
+        <circle fill="black" cx="14" cy="14" r="7"></circle>
+    </svg>`;
+
+    // Gen random data
+    const N = 30;
+    const gData: MarkerData[] = Array.from(Array(N).keys()).map(() => ({
+        lat: (Math.random() - 0.5) * 180,
+        lng: (Math.random() - 0.5) * 360,
+        size: 7 + Math.random() * 30,
+        color: ['red', 'white', 'blue', 'green'][Math.round(Math.random() * 3)]
+    }));
+        
     return (
       <div className="globe-map">
         <button onClick={() => setSimSpeed(1)}>1x Speed</button>
@@ -67,6 +89,24 @@ export const GlobeMap: React.FC<{ gameState: GameState; updateGameState: (newSta
         {currentTickets}
         <div className="map-container">
             {cityButtons}
+            <Globe
+            globeImageUrl="//cdn.jsdelivr.net/npm/three-globe/example/img/earth-dark.jpg"
+            htmlElementsData={gData}
+            htmlElement={(d: object) => {
+                const markerData = d as MarkerData;
+                const el = document.createElement('div');
+                el.innerHTML = markerSvg;
+                el.style.color = markerData.color;
+                el.style.width = `${markerData.size}px`;
+                el.style.transition = 'opacity 250ms';
+
+                el.style.setProperty('pointer-events', 'auto');
+                el.style.cursor = 'pointer';
+                el.onclick = () => console.info(markerData);
+                return el;
+            }}
+            htmlElementVisibilityModifier={(el, isVisible) => el.style.opacity = isVisible ? '1' : '0'}
+        />
         </div>
       </div>
     );
